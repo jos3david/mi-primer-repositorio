436e83eb0721fa2355e58d3dab753ab26c22453c
jos3david <torres.manrique@22gmail.com>
2024-10-16 17:58:31 -0400

    mensaje

---
+++ arrays.js
@@ -0,0 +1,4 @@
+let arreglo = ["jose", 15, "pizza2", true, false]
+console.log(arreglo[1])
+
+
---
+++ repaso.js
@@ -0,0 +1,16 @@
+
+function adivinaLaFruta (fruta) {
+ if (fruta == "manzana") {
+    return "si"
+ } else {
+    return "no" 
+ }
+} 
+    
+console.log(adivinaLaFruta ("manzana"))
+
+
+
+
+
+
